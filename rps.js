a45daba688930518b2908595232a8140373c8401
Ragnov <vicente35713@gmail.com>
2022-11-16 17:05:21 +0800

    add getComputerChoice

--- rps.js
+++ rps.js
@@ -1,1 +1,7 @@
-console.log('test');
+function getComputerChoice(){
+    const answer = ['Rock', 'Paper' , 'Scissor'];
+    const randomElement = Math.floor(Math.random() * answer.length);
+    return answer[randomElement];
+};
+
+console.log(getComputerChoice())
